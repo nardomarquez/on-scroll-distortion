b36e20d03769891f27b01ec21da26ad34be46175
Rennard Marquez <marquezrennard@gmail.com>
2024-09-09 16:46:54 +0800

    add Item class and render them

---
+++ src/webgl/Item.ts
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+import { Sizes } from "../lib/types";
+
+interface ItemProps {
+  image: HTMLImageElement;
+  sizes: Sizes;
+  currentScroll: number;
+}
+
+export default class Item {
+  image: HTMLImageElement;
+  sizes: Sizes;
+  imageData: {
+    image: HTMLImageElement;
+    mesh: THREE.Mesh;
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+  } | null = null;
+  currentScroll: number;
+
+  constructor({ image, sizes, currentScroll }: ItemProps) {
+    this.image = image;
+    this.sizes = sizes;
+    this.currentScroll = currentScroll;
+
+    // methods
+    this.createImage();
+    this.render();
+
+    // events
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  createImage() {
+    let bounds = this.image.getBoundingClientRect();
+    let geometry = new THREE.PlaneGeometry(1, 1, 16, 16);
+    let texture = new THREE.TextureLoader().load(this.image.src);
+    let material = new THREE.MeshBasicMaterial({
+      side: THREE.DoubleSide,
+      map: texture,
+      transparent: true,
+      wireframe: true,
+    });
+    const imageMesh = new THREE.Mesh(geometry, material);
+    imageMesh.scale.set(bounds.width, bounds.height, 1);
+    this.imageData = {
+      image: this.image,
+      mesh: imageMesh,
+      top: bounds.top,
+      left: bounds.left,
+      width: bounds.width,
+      height: bounds.height,
+    };
+  }
+
+  updatePosition() {
+    if (this.imageData) {
+      this.imageData.mesh.position.x =
+        this.imageData.left - this.sizes.width / 2 + this.imageData.width / 2;
+      this.imageData.mesh.position.y =
+        this.currentScroll - this.imageData.top + this.sizes.height / 2 - this.imageData.height / 2;
+    }
+  }
+
+  updateImageData() {
+    if (this.imageData) {
+      let bounds = this.image.getBoundingClientRect();
+      this.imageData.top = bounds.top + this.currentScroll;
+      this.imageData.left = bounds.left;
+      this.imageData.width = bounds.width;
+      this.imageData.height = bounds.height;
+      this.imageData.mesh.scale.set(this.imageData.width, this.imageData.height, 1);
+    }
+  }
+
+  resize() {
+    this.sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  render() {
+    this.updatePosition();
+    this.updateImageData();
+  }
+}
--- src/webgl/WebGL.ts
+++ src/webgl/WebGL.ts
@@ -1,64 +1,93 @@
 import * as THREE from "three";
+import Item from "./Item";
+import Lenis from "lenis";
+import { Sizes } from "../lib/types";
 
 const cameraDistance = 1;
 
 export default class WebGL {
-  width: number;
-  height: number;
+  sizes: Sizes;
   scene: THREE.Scene;
   camera: THREE.PerspectiveCamera;
   renderer: THREE.WebGLRenderer;
+  lenis: Lenis;
+  currentScroll: number;
+  items: Item[];
 
   constructor() {
+    // scroll init
+    window.scrollTo(0, 0);
+    this.lenis = new Lenis();
+    this.currentScroll = this.lenis.scroll;
+
     // init
     const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
-    this.width = canvas.width;
-    this.height = canvas.height;
+    this.sizes = { width: window.innerWidth, height: window.innerHeight };
 
     // Scene
     this.scene = new THREE.Scene();
 
     // Camera
-    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.1, 100);
-    this.camera.fov = 2 * Math.atan(this.height / 2 / cameraDistance) * (180 / Math.PI);
+    this.camera = new THREE.PerspectiveCamera(70, this.sizes.width / this.sizes.height, 0.1, 100);
+    this.camera.fov = 2 * Math.atan(this.sizes.height / 2 / cameraDistance) * (180 / Math.PI);
     this.camera.position.set(0, 0, cameraDistance);
 
     // Renderer
     this.renderer = new THREE.WebGLRenderer({
       canvas: canvas,
       antialias: true,
-      //   alpha: true,
+      alpha: true,
     });
-    this.renderer.setSize(this.width, this.height);
+    this.renderer.setSize(this.sizes.width, this.sizes.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+    // create items
+    let images = [...document.querySelectorAll("img")];
+    this.items = this.createItems(images);
+
     // methods
-    this.createCube();
     this.resize();
     this.render();
 
     // events
-    window.addEventListener("resize", () => this.resize());
+    window.addEventListener("resize", this.resize.bind(this));
+    window.addEventListener("scroll", () => {
+      this.currentScroll = this.lenis.scroll;
+    });
   }
 
-  createCube() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
+  createItems(images: HTMLImageElement[]): Item[] {
+    return images.map((image) => {
+      const item = new Item({
+        image: image,
+        sizes: { width: this.sizes.width, height: this.sizes.height },
+        currentScroll: this.currentScroll,
+      });
+
+      if (item.imageData) {
+        this.scene.add(item.imageData.mesh);
+      }
+
+      return item;
+    });
   }
 
   resize() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.camera.aspect = this.width / this.height;
-    this.camera.fov = 2 * Math.atan(this.height / 2 / cameraDistance) * (180 / Math.PI);
-    this.renderer.setSize(this.width, this.height);
+    this.sizes.width = window.innerWidth;
+    this.sizes.height = window.innerHeight;
+    this.camera.aspect = this.sizes.width / this.sizes.height;
+    this.camera.fov = 2 * Math.atan(this.sizes.height / 2 / cameraDistance) * (180 / Math.PI);
+    this.renderer.setSize(this.sizes.width, this.sizes.height);
     this.camera.updateProjectionMatrix();
   }
 
   render() {
-    requestAnimationFrame(() => this.render());
-    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame((e) => {
+      this.lenis.raf(e);
+      this.items.forEach((item) => item.render());
+      this.renderer.render(this.scene, this.camera);
+
+      this.render();
+    });
   }
 }
